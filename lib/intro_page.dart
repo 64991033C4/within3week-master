08be389adf51d3972618031f3603d969c063e9a5
Chamel <chatmethar2@gmail.com>
2024-09-02 23:26:46 +0700

    add google map to intro

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -1,42 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:within3week/screen/journey.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const LatLng kosenkmitl = LatLng(13.72699, 100.7789);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome'),
+        title: const Text(
+          'Event Subscriber Service',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Container(
+              margin: const EdgeInsets.all(5.0),
+              width: 35.0,
+              height: 35.0,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2.0,
+                ),
+              ),
+              child: const Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+            ),
+            onPressed: () {
+              // Handle user profile button press
+              Navigator.of(context).pushReplacementNamed('/login');
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Our App!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-
-                if (user != null) {
-                  // User is logged in, navigate to the Journey page
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    '/journey',
-                    (Route<dynamic> route) => false,
-                    arguments: Journey(),
-                  ); // or 'Teacher', depending on the user's role
-                } else {
-                  // User is not logged in, navigate to the LoginPage
-                  Navigator.of(context).pushReplacementNamed('/login');
-                }
-              },
-              child: Text('Get Started'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '       KOSEN-KMITL is an educational institution that was established according to '
+                      'the project to develop manpower in engineering, technology, and innovation. '
+                      'Divided into three branches: ',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      '• Mechatronics Engineering',
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '• Computer Engineering',
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '• Electrical Engineering and Electronic Engineering',
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 15),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text(
+                      'KOSEN-KMITL Location',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 300,
+                      width: double.infinity,
+                      child: GoogleMap(
+                        initialCameraPosition: const CameraPosition(
+                          target: kosenkmitl,
+                          zoom: 15,
+                        ),
+                        markers: {
+                          const Marker(
+                            markerId: MarkerId('KosenKmitl'),
+                            position: kosenkmitl,
+                            infoWindow: InfoWindow(title: 'KOSEN-KMITL'),
+                          ),
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Coordinates: 13.72699, 100.7789',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
